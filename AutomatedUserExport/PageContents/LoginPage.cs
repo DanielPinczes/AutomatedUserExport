331ae8c9cd4e46d714b8c294261555ce28ce1ce9
Dániel Pinczés <daniel.pinczes@outlook.com>
2018-03-17 14:41:31 +0100

    Added Login Page elements and methods
    Included html elements of login page and the methods which contain the operations with elements.

---
+++ AutomatedUserExport/PageContents/LoginPage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using AutomatedUserExport;
+using AutomatedUserExport.HelperClasses;
+
+namespace AutomatedUserExport.PageContents
+{
+	class LoginPage
+	{
+		IWebDriver driver;
+		SecretDetailsReader sdr;
+
+		public IWebDriver Driver { set => driver = value; }
+		public SecretDetailsReader Sdr { set => sdr = value; }
+
+		By userNameTxbx = By.CssSelector("input[name = usrname]");
+		By passwordTxbx = By.CssSelector("input[name = pass]");
+		By submitBtn = By.CssSelector("input[name = submit]");
+
+		IWebElement UserNameTxbx() => driver.FindElement(userNameTxbx);
+
+		IWebElement PasswordTxbx() => driver.FindElement(passwordTxbx);
+
+		IWebElement SubmitBtn () => driver.FindElement(submitBtn);
+
+		void SetUserName() => UserNameTxbx().SendKeys(sdr.GetSecretValue("username"));
+
+		void SetPassword() => PasswordTxbx().SendKeys(sdr.GetSecretValue("password"));
+
+		void ClickSubmit() => SubmitBtn().Click();
+
+		public void Login()
+		{
+			driver.Navigate().GoToUrl(sdr.GetSecretValue("loginURL"));
+			SetUserName();
+			SetPassword();
+			ClickSubmit();
+		}
+		
+	}
+}
